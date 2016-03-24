591462eab2bcbf985567578ec7bf2a3f48a4cce5
LionelLints <lionel.lints@gmail.com>
2016-03-23 21:07:17 -0700

    basic server built for testing

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var express = require(express);
-var app = express();
+'use strict';
+
+const express = require(express);
+const app = express();
+const http = require('http');
+
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on', app.get('port'));
+});
