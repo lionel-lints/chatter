ca9df291c1caa059f1c30191845f644dfd34df65
LionelLints <lionel.lints@gmail.com>
2016-03-25 06:30:26 -0700

    updates

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+http.createServer(function(){});
+const io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 app.disable('x-powered-by');
 app.set('port', process.env.PORT || 5000);
@@ -11,6 +17,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
+http.listen(app.get('port'), function() {
   console.log('Listening on', app.get('port'));
 });
